d2b3011996e3c56f60e450e1030bf173d0f25421
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-27 19:53:36 +0200

    added initial ontext

---
+++ src/context.js
@@ -0,0 +1,16 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { useCallback } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value="hello world">{children}</AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
