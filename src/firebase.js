af0371c3dbad1a34441ea771394ba6ee6e7ec528
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-27 19:44:29 +0200

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCdoesCw2Y1wTz1CfR2RzYWM806tDodALY',
+  authDomain: 'cook-uni-c0938.firebaseapp.com',
+  databaseURL: 'https://cook-uni-c0938-default-rtdb.firebaseio.com',
+  projectId: 'cook-uni-c0938',
+  storageBucket: 'cook-uni-c0938.appspot.com',
+  messagingSenderId: '917398348811',
+  appId: '1:917398348811:web:b7afdcd7ba6e4e70d3930e',
+};
