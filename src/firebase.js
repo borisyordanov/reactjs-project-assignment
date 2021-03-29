b146efd8c7c4f0900e89e61b4443eff58ca95424
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-29 15:43:47 +0300

    installed firebase and updated config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyCdoesCw2Y1wTz1CfR2RzYWM806tDodALY',
   authDomain: 'cook-uni-c0938.firebaseapp.com',
@@ -7,3 +9,9 @@ const firebaseConfig = {
   messagingSenderId: '917398348811',
   appId: '1:917398348811:web:6b224d9686f70d15d3930e',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
