aab7ac4a64ca1664d888156dbb39c68c49062510
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-29 17:55:41 +0300

    adding basic styling and header nav

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import SingleRecipe from './pages/SingleRecipe';
-// import Error from './pages/Error';
-// import Register from './pages/Register';
-// import Login from './pages/Login';
+import Home from './pages/Home';
+import SingleRecipe from './pages/SingleRecipe';
+import Error from './pages/Error';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
-// import Navbar from './components/Navbar';
-import Loading from './components/Loading';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Loading />
-        {/* <Navbar /> */}
-        <Switch>
-          {/* <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/recipe/:id">
-            <SingleRecipe />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="*">
-            <Error />
-          </Route> */}
-        </Switch>
-      </Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/recipe/:id">
+          <SingleRecipe />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,61 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 function Navbar() {
-  return <div></div>;
+  return (
+    // <nav classNameName="navbar">
+    //   <Link to="/">
+    //     <img
+    //       className="nav-logo"
+    //       src="http://cdn.onlinewebfonts.com/svg/img_415179.png"
+    //       alt="Cookbook"
+    //     />
+    //   </Link>
+    //   <div className="nav">
+    //     <Link to="/login">
+    //       <div className="nav-option">
+    //         <span className="nav-optionLineOne">Hello Guest</span>
+    //         <span className="nav-optionLineTwo">Login</span>
+    //       </div>
+    //     </Link>
+    //     <Link to="/register">
+    //       <div className="nav-option">
+    //         <span className="nav-optionLineTwo">Register</span>
+    //       </div>
+    //     </Link>
+    //   </div>
+    // </nav>
+
+    // <div className="header">
+    //   <Link to="/" className="header-link">
+    //     The Cook Book
+    //   </Link>
+    //   <div className="header-right">
+    //     <Link className="active" to="/">
+    //       Home
+    //     </Link>
+    //     <Link to="/login">Login</Link>
+    //     <Link to="/register">Register</Link>
+    //   </div>
+    // </div>
+
+    <div className="topnav">
+      <NavLink to="/">
+        <img
+          className="logo"
+          src="http://cdn.onlinewebfonts.com/svg/img_415179.png"
+          alt="Cookbook"
+        />
+      </NavLink>
+      <div className="links-nav">
+        <NavLink className="link" to="/login">
+          Login
+        </NavLink>
+        <NavLink className="link" to="register">
+          Register
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
