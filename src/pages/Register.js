fcbd9803d385386accac6f5d6323b85f11df81e1
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-30 18:02:47 +0300

    Register implemented

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { auth } from '../firebase';
 
 function Register() {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+
+  const register = (e) => {
+    e.preventDefault();
+    if (password === repeatPassword) {
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then((auth) => {
+          console.log(auth);
+          history.push('/');
+        })
+        .catch((error) => alert(error.message));
+    } else {
+      setPassword('');
+      setRepeatPassword('');
+      alert('Passwords must match!');
+    }
+  };
+
   return (
     <>
       <form className="text-center p-5 form-layout">
@@ -12,6 +36,8 @@ function Register() {
           className="form-control mb-4"
           name="email"
           placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
@@ -19,6 +45,8 @@ function Register() {
           className="form-control"
           name="password"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <input
@@ -27,10 +55,13 @@ function Register() {
           className="form-control"
           name="repeatPassword"
           placeholder="Repeat Password"
+          value={repeatPassword}
+          onChange={(e) => setRepeatPassword(e.target.value)}
         />
         <button
           className="btn btn-danger my-4 btn-block w-25 m-auto"
           type="submit"
+          onClick={register}
         >
           Sign up
         </button>
