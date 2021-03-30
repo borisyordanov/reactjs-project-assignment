5af797a577a340f65d71286fbbe0f3a07da952aa
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-03-30 14:44:57 +0300

    updated the error page CSS

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -6,11 +6,14 @@ function Error() {
     <section className="error-page section">
       <div className="error-container">
         <h1>oops! it's a dead end</h1>
-        <img
-          src="https://i.pinimg.com/originals/24/8c/c4/248cc4eec11b158d6eaf49c7088022a4.jpg"
-          alt="404 not found"
-        />
-        <Link to="/" className="button">
+        <div>
+          <img
+            className="errorImg"
+            src="https://i.pinimg.com/originals/24/8c/c4/248cc4eec11b158d6eaf49c7088022a4.jpg"
+            alt="404 not found"
+          />
+        </div>
+        <Link to="/" className="btn btn-danger">
           back home
         </Link>
       </div>
